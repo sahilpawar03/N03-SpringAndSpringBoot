fe211635a4cb829b6022d3113ddb46c0f95e6a8c
chintamani07 <chintamaniwankhede123@gmail.com>
2023-03-14 07:20:33 +0530

    First Commit

---
+++ SpringIOC/src/main/java/org/tnsif/springioc/Airtel.java
@@ -0,0 +1,12 @@
+package org.tnsif.springioc;
+//
+public class Airtel implements Sim{
+
+	public void call() {
+		System.out.println("call using Airtel");
+	}
+	public void message () {
+		System.out.println("6G is coming soon....");
+	}
+	
+}
---
+++ SpringIOC/src/main/java/org/tnsif/springioc/MobileExecutor.java
@@ -0,0 +1,35 @@
+package org.tnsif.springioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// program to demonstrate on spring ioc
+public class MobileExecutor {
+
+	public static void main(String[] args) {
+		Airtel a=new Airtel();
+		a.call();
+		a.message();
+		
+		Jio j=new Jio();
+		j.call();
+		j.message();
+		
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		
+		Airtel a1=(Airtel)c.getBean("airtel");
+		a1.call();
+		a1.message();
+		
+		/*Jio a2=(Jio)c.getBean("jio");
+		a2.call();
+		a2.message();*/
+		
+		Sim s=c.getBean("sim",Sim.class);
+		s.call();
+		s.message();
+		
+		
+	}
+
+}
